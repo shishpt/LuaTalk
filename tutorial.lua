0fba78c8c332014aa84293881efe88ae03c4dd43
shishir <shishirpatel@gmail.com>
2023-04-24 09:08:05 +0000

    tutorial

---
+++ tutorial.lua
@@ -0,0 +1,20 @@
+--comment
+--[[
+    many lines
+]]
+
+--print
+print("End of days")
+
+--variables
+local school = "Tskoli"
+local students = "300"
+
+print(school)
+print(students)
+
+local function module ()
+    print("module7")
+end
+
+module()
